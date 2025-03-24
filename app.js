4d37267a1859c677270b064129483dc05127000a
Tomas Steven Desktop <tomssteven22@gmail.com>
2025-03-24 17:58:00 -0300

    servidor activo

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const {
-  S3Client,
-  DeleteObjectCommand,
-  PutObjectCommand,
-} = require("@aws-sdk/client-s3");
-
 const cookieParser = require("cookie-parser");
 
 const sistema = require("./Routes/sistema.route");
@@ -16,6 +10,7 @@ const secretaria = require("./Routes/Secretaria.router");
 
 const app = express();
 const dotenv = require("dotenv");
+const cron = require("node-cron");
 
 dotenv.config();
 
@@ -26,6 +21,31 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use(express.static(__dirname + "/uploads"));
+
+// Cron Jobs
+cron.schedule("0 */2 * * *", async () => {
+  try {
+    const res = await fetch(
+      "https://exuberant-joelle-digitalcode-937bee37.koyeb.app/api/pacientes",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: process.env.token,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    console.log("Cron Job executed");
+  } catch (error) {
+    console.error("Error in Cron Job:", error);
+  }
+});
 
 // Routes
 app.use("/api", paciente);
